8e891e99670e7570b49cd49d8aeff6d01f4a3d4d
Nayeli Jordan <[nayeli@queonda.com.mx]>
2019-03-28 17:57:16 -0600

    inicio diseño gráfico

--- themes/vyarte/page-diseno-grafico.php
+++ themes/vyarte/page-diseno-grafico.php
@@ -1,8 +1,25 @@
 <?php get_header(); 
 	if (have_posts()) : while (have_posts()) : the_post();?>
-		<section class="container  padding-top-bottom-30">
-			<h2 class="title-section"><span><?php the_title(); ?></span></h2>
-			<?php the_content(); ?>
+		<section>
+			<div class="bg-category bg-image margin-bottom-30" style="background-image: url(<?php the_post_thumbnail_url('full'); ?>)">
+				<div class="bg-opacity-dark bg-absolute"></div>
+				<div class="content-center text-center padding-left-right-20 color-light">
+					<h2 class="margin-bottom-10"><?php the_title(); ?></h2>
+					<?php the_content(); ?>					
+				</div>
+			</div>
+			<div class="container">
+				<h3 class="margin-bottom-30">Conoce nuestro trabajo</h3>
+				<h3 class="margin-bottom-30">Servicios de Diseño Gráfico</h3>
+				<?php echo do_shortcode('[products category="diseno-grafico"]'); ?>
+			</div>
+			<div class="bg-gray text-center padding-top-bottom-40">
+				<div class="container">
+					<p class="uppercase">¿No encuentras el servicio ideal que necesitas?</p>
+					<p class="margin-bottom-20">Contáctanos y te ofreceremos una cotización</p>
+					<a href="<?php echo SITEURL; ?>contacto" class="btn">Contacto</a>	
+				</div>
+			</div>
 		</section>
 	<?php endwhile; endif; 
 get_footer(); ?>
--- themes/vyarte/page-sublimacion-y-serigrafia.php
+++ themes/vyarte/page-sublimacion-y-serigrafia.php
@@ -4,7 +4,7 @@
 			<div class="bg-category bg-image margin-bottom-30" style="background-image: url(<?php the_post_thumbnail_url('full'); ?>)">
 				<div class="bg-opacity-dark bg-absolute"></div>
 				<div class="content-center text-center padding-left-right-20 color-light">
-					<h2 class="uppercase margin-bottom-10"><?php the_title(); ?></h2>
+					<h2 class="margin-bottom-10"><?php the_title(); ?></h2>
 					<?php the_content(); ?>					
 				</div>
 			</div>
