e5fea8f97e1c50a282e08588fb110cbbbaf5b71d
Nayeli Jordan <[nayeli@queonda.com.mx]>
2019-04-10 18:03:33 -0500

    nota personalizar en mail

--- themes/vyarte/woocommerce/emails/customer-processing-order.php
+++ themes/vyarte/woocommerce/emails/customer-processing-order.php
@@ -57,7 +57,7 @@ do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_
 	<p>Entra al siguiente enlace si aún no has personalizado tu producto*, entre más pronto realices este paso antes podremos hacerte entrega de tu pedido.</p>
 	<p>De la misma manera, si deseas que no hagamos ningún tipo de personalización indícanoslo seleccionando "No deseo personalizar".</p>
 	<a href="<?php echo SITEURL; ?>cuenta/view-order/<?php echo $noOrder; ?>">Personalizar producto</a><br>
-	<p><small>Sólo para productos personalizables de sublimación o serigrafía. No aplica en productos de diseño gráfico.</small></p><br>	
+	<p><small>*Sólo para productos personalizables de sublimación o serigrafía. No aplica en productos de diseño gráfico. Cuentas con hasta 24hr. para personalizar tu producto a partir de tu compra, de lo contrario prepararemos tu pedido para su envío sin realizar ningún cambio.</small></p><br>	
 </article>
 
 <p>
